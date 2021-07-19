b081aa7db6659188bc7d1bdac93b3c7f8f4a10bf
TRAMPlin30 <vicodin28@gmail.com>
2021-07-19 12:30:27 +0300

    added files: index.html, products.html, (edit static-root)

--- my_store/urls.py
+++ my_store/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import index
+from products.views import products
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name = 'index'),
+    path('products/', products, name = 'products'),
 ]
